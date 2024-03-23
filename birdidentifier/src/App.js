2ad3e3bf5497e6be9acf4049fe5b40d352981473
mustafahawa004 <124458719+mustafahawa004@users.noreply.github.com>
2024-03-23 18:42:36 +0000

    react setup

--- birdidentifier/src/App.js
+++ birdidentifier/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isTranslated, setIsTranslated] = useState(false);
+  const [direction, setDirection] = useState('left');
+  const [showAboutText, setShowAboutText] = useState(false);
+
+  const handleTranslate = () => {
+    setIsTranslated(true);
+    setDirection('left');
+  };
+
+  const handleGoHome = () => {
+    setIsTranslated(true);
+    setDirection('right');
+  };
+
+  const handleShowAboutText = () => {
+    setShowAboutText(true);
+  };
+
+  const handleHideAboutText = () => {
+    setShowAboutText(false);
+  };
+
   return (
     <div className="App">
+      {/* Violet horizontal bar at the top */}
+      <div className="violet-horizontal-bar">
+        {/* Buttons */}
+        <button className="how-to-use-button" onClick={() => { handleGoHome(); handleHideAboutText(); }}>Home Page</button>
+        <button className="about-button" onClick={() => { handleTranslate(); handleShowAboutText(); }}>About</button>
+        <button className="contact-us-button" onClick={() => { handleTranslate(); handleHideAboutText(); }}>Contact Us</button>
+      </div>
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Logo */}
+        <img src="/bird-explorer.png" alt="Bird Explorer Logo" className={`App-logo ${isTranslated ? `translate-${direction}` : ''}`} />
+
+        {/* About text */}
+        {showAboutText && (
+          <div className="about-text">
+            <h2>About Us:</h2>
+            <p>"We are a collective of Virginia Tech Students advocating for fellow students to have better access to nutritional information about their food. Our goal is to empower students to monitor and understand what they consume, as we firmly believe that prioritizing health ultimately leads to wealth."</p>
+          </div>
+        )}
       </header>
+
+      {/* Violet horizontal bar at the bottom */}
+      <div className="violet-horizontal-bar"></div>
     </div>
   );
 }
